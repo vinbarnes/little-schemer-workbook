ba0be7bdd635c7751599a89e2aa0b91b23b4d905
Kevin R. Barnes <vinbarnes@gmail.com>
2017-10-02 05:58:29 -0500

    notes from chapter 2

---
+++ chapter2.rkt
@@ -0,0 +1,14 @@
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat))))
+)))
